b84bf17e5b1d573a5c23bcb931b47c8ed438e5d0
Jaren24 <56574736+Jaren24@users.noreply.github.com>
2019-12-03 09:56:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/carloancalculator/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.carloancalculator
+
+//App coded by: Jaren Yeap Wei Loon
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        buttonCalculate.setOnClickListener {
+            textViewLoan.setText("Loan :")
+            textViewInterest.setText("Interest :")
+            textViewMonthlyRepayment.setText("Monthly Repayment :")
+            val cp = editTextCarPrice.text.toString()
+            val dp = editTextDownPayment.text.toString()
+            val lp = editTextLoanPeriod.text.toString()
+            val ir = editTextInterestRate.text.toString()
+            var carLoan = 0
+            var interest = 0
+            var monthlyRepayment = 0
+            if(cp.toString().equals("") || dp.toString().equals("") || lp.toString().equals("") || ir.toString().equals("")) {
+                Toast.makeText(getApplicationContext(), "Please fill in the detail to calculate.", Toast.LENGTH_SHORT).show()
+                println("toasted.")
+            }
+            else {
+                carLoan = cp.toInt() - dp.toInt()
+                interest = carLoan * ir.toInt() * lp.toInt()
+                monthlyRepayment = (carLoan + interest) / lp.toInt() / 12
+                textViewLoan.append(carLoan.toString())
+                textViewInterest.append(interest.toString())
+                textViewMonthlyRepayment.append(monthlyRepayment.toString())
+            }
+        }
+        buttonReset.setOnClickListener {
+            editTextCarPrice.setText("")
+            editTextDownPayment.setText("")
+            editTextInterestRate.setText("")
+            editTextLoanPeriod.setText("")
+            textViewLoan.setText("Loan :")
+            textViewInterest.setText("Interest :")
+            textViewMonthlyRepayment.setText("Monthly Repayment :")
+
+        }
+    }
+}
